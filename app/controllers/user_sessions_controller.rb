be010bd5b39f97826a74f2a59b4d4bb151f5bdf2
Vicent Gozalbes <vigosan@gmail.com>
2009-03-16 14:51:12 +0100

    fix problem login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,13 +8,11 @@ class UserSessionsController < ApplicationController
   
   def create
     @user_session = UserSession.new(params[:user_session])
-    @user_session.save do |result|
-      if result
-        flash[:notice] = "Login successful!"
-        redirect_back_or_default root_url
-      else
-        render :action => :new
-      end
+    if @user_session.save
+      flash[:notice] = "Login successful!"
+      redirect_back_or_default account_url
+    else
+      render :action => :new
     end
   end
   
