657d0a7c11788d8a7fc7fa8bbec89a77281f1a78
Vicent Gozalbes <vigosan@gmail.com>
2009-03-17 22:38:47 +0100

    locales and style

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,7 @@ class Profile < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "100x100>", :thumb => "50x50>", :micro => "25x25>" },
     :url  => "/avatars/:id/:style/:basename.:extension",
     :path => ":rails_root/public/avatars/:id/:style/:basename.:extension",
-    :default_url => "/images/avatars/:style/missing.png"          
+    :default_url => "/images/avatars/:style/missing.png"
     
   def full_name
     [first_name, last_name].delete_if(&:blank?).join(" ")
