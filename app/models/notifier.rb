b4dda6bc0e61cee285ca0cff1488a50d36a9dbaa
Vicent Gozalbes <vigosan@gmail.com>
2009-03-18 09:30:52 +0100

    added app domain in config

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = APP_CONFIG["app_url"]
+  default_url_options[:host] = APP_CONFIG["app_domain"]
 
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
