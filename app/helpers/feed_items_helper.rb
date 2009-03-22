5cfd429e5ef4a0c2bf57f0ee6705fae1217cc375
Vicent Gozalbes <vigosan@gmail.com>
2009-03-22 22:43:38 +0100

    locales

--- app/helpers/feed_items_helper.rb
+++ app/helpers/feed_items_helper.rb
@@ -3,13 +3,14 @@ module FeedItemsHelper
     item = feed_item.item
     case item.class.name
     when "Profile"
-      "#{link_to item.profile.user, user_profile_path(item.profile.user.login)} has updated his profile"
+      t("helpers.feed_items.profile", :login => (link_to item.profile.user, user_profile_path(item.profile.user.login)))
     when "Avatar"
-      "#{link_to item.profile.user, user_profile_path(item.profile.user.login)} has changed his avatar"
+      t("helpers.feed_items.avatar", :login => (link_to item.profile.user, user_profile_path(item.profile.user.login)))
     when "Friend"
       inviter = item.inviter.user
       invited = item.invited.user
-      "#{link_to inviter, user_profile_path(inviter.login)} and #{link_to invited, user_profile_path(invited.login)} are friends"
+      t("helpers.feed_items.are_friends", :inviter => (link_to inviter, user_profile_path(inviter.login)), 
+        :invited => (link_to invited, user_profile_path(invited.login)))
     end
   end
 end
--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -2,7 +2,7 @@ module FriendsHelper
   def friend_link(inviter, invited)
     return link_to t("views.friends.log_in_to_follow"), login_path if inviter.blank?
     return '' if !(inviter && invited) or (inviter.id == invited.id)
-    return link_to 'Stop Being Friends', break_path(invited.user.login), :method => :delete if invited.followed_by?(inviter)
-    link_to "Start Following", confirm_path(invited.user.login), :method => :post
+    return link_to t("helpers.friends.stop", :invited => invited.user.login), break_path(invited.user.login), :method => :delete if invited.followed_by?(inviter)
+    link_to t("helpers.friends.start", :invited => invited.user.login), confirm_path(invited.user.login), :method => :post
   end
 end
